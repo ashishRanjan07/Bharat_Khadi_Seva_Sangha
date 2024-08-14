ed320e36cf0c8c963577198a779ca363269e926b
ashishranjan9629 <ashish.ranjan@velocis.co.in>
2024-08-14 12:34:42 +0530

    Added Contact us and About us Webview

--- src/screen/appScreen/drawerScreen/AboutUs.js
+++ src/screen/appScreen/drawerScreen/AboutUs.js
@@ -1,12 +1,29 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {StatusBar, StyleSheet, Text, View} from 'react-native';
+import React, {useState} from 'react';
 import CustomHeader from '../../../components/CustomHeader';
 import {AppColor} from '../../../utils/AppColor';
+import {WebView} from 'react-native-webview';
+import {BarIndicator} from 'react-native-indicators';
 
 const AboutUs = () => {
+  const [isLoading, setIsLoading] = useState(true);
   return (
     <View style={styles.main}>
+      <StatusBar barStyle={'dark-content'} backgroundColor={AppColor.white} />
       <CustomHeader title={'About Us'} />
+      {isLoading && (
+        <View style={styles.loaderContainer}>
+          <BarIndicator color={AppColor.primary} />
+        </View>
+      )}
+      <WebView
+        source={{
+          uri: 'https://www.khadindia.com/about/',
+        }}
+        onLoadStart={() => setIsLoading(true)}
+        onLoadEnd={() => setIsLoading(false)}
+        style={isLoading ? {display: 'none'} : {flex: 1}}
+      />
     </View>
   );
 };
@@ -16,6 +33,16 @@ export default AboutUs;
 const styles = StyleSheet.create({
   main: {
     flex: 1,
+    backgroundColor: AppColor.white,
+  },
+  loaderContainer: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: 'center',
+    alignItems: 'center',
     backgroundColor: AppColor.white,
   },
 });
--- src/screen/appScreen/drawerScreen/ContactUs.jsx
+++ src/screen/appScreen/drawerScreen/ContactUs.jsx
@@ -1,21 +1,48 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { AppColor } from '../../../utils/AppColor'
-import CustomHeader from '../../../components/CustomHeader'
+import {StatusBar, StyleSheet, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {AppColor} from '../../../utils/AppColor';
+import CustomHeader from '../../../components/CustomHeader';
+import {WebView} from 'react-native-webview';
+import {BarIndicator} from 'react-native-indicators';
 
 const ContactUs = () => {
+  const [isLoading, setIsLoading] = useState(true);
   return (
     <View style={styles.main}>
-    <CustomHeader title={"Contact Us"}/>
+      <StatusBar backgroundColor={AppColor.white} barStyle={'dark-content'} />
+      <CustomHeader title={'Contact Us'} />
+      {isLoading && (
+        <View style={styles.loaderContainer}>
+          <BarIndicator color={AppColor.primary} />
+        </View>
+      )}
+      <WebView
+        source={{
+          uri: 'https://www.khadindia.com/contact/',
+        }}
+        onLoadStart={() => setIsLoading(true)}
+        onLoadEnd={() => setIsLoading(false)}
+        style={isLoading ? {display: 'none'} : {flex: 1}}
+      />
     </View>
-  )
-}
+  );
+};
 
-export default ContactUs
+export default ContactUs;
 
 const styles = StyleSheet.create({
-  main:{
-    flex:1,
-    backgroundColor:AppColor.white
-  }
-})
+  main: {
+    flex: 1,
+    backgroundColor: AppColor.white,
+  },
+  loaderContainer: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: AppColor.white,
+  },
+});
